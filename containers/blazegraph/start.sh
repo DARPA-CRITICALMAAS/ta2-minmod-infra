a3365308592b1a80ab18ecc27537d970ed14105f
Binh Vu <bvu687@gmail.com>
2025-01-07 16:23:55 -0800

    remove invalid containers

--- containers/blazegraph/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-# Start Blazegraph
-java -server $JVM_ARGS -Dbigdata.propertyFile=/home/criticalmaas/config/blazegraph.properties -jar /home/criticalmaas/blazegraph.jar
--- containers/virtuoso/setup.sql
+++
@@ -1,3 +0,0 @@
-grant execute on DB.DBA.SPARQL_MODIFY_BY_DICT_CONTENTS to "SPARQL" ;
-grant SPARQL_UPDATE to "SPARQL" ;
-DB.DBA.RDF_GRAPH_USER_PERMS_SET ('https://minmod.isi.edu', 'SPARQL', 3);
